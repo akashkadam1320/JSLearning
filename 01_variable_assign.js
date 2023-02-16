a898974fa990280307c9c30d2e90e6ba62713209
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-16 14:32:23 +0530

    variable declaration and initialization
    variable declaration and initialization

---
+++ 01_variable_assign.js
@@ -0,0 +1,27 @@
+console.log("************* step1 *************");
+var  myFullName;
+console.log(myFullName);
+myFullName = " akash kadam";
+console.log("My Full Name is", myFullName);
+myFullName = "akash rajkumar kadam"
+console.log("My first Name, Middle Name and Last Name",myFullName);
+
+console.log("************* step2 *************");
+var myLove ="I Love JS"
+console.log(myLove);
+
+console.log("************* step3 *************");
+var firstName = "akash";
+var lastName ="kadam";
+var age ="26";
+var addres ="mumbai";
+var pincode = "400072";
+var gender ="male";
+var isMarried ="no";
+console.log("First name is:",firstName);
+console.log("Last name is:",lastName);
+console.log("My age:",age);
+console.log("Addres:",addres);
+console.log("Pincode:",pincode);
+console.log("Gender:",gender);
+console.log("Marride status:",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,20 @@
+// personal details
 console.log("mohit sharma");
+console.log("31");
+console.log("mumbai");
+
+var fullName ="mohit sharma";
+console.log(fullName);
+console.log(fullName);
+
+ var city; //variable declaration
+console.log(city);
+city = "mumbai"; //variable initialization
+console.log(city);
+city ="surat";
+console.log(city);
+
+var age = "31";  // variable declaration and initialzation in the same line
+age = 32;
+age = 33;
+console.log(age);
