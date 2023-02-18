b06ed00f995f355fa5fa866e3ca80130237c4c85
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-19 01:12:04 +0530

    typeOFAssigment
    typeOFAssigment

---
+++ 02_typeOfAssig.js
@@ -0,0 +1,25 @@
+console.log("********(1)********");
+
+var bankName;
+bankName ="SBI Bank";
+console.log(typeof bankName);
+
+console.log("********(2)********");
+
+var marks ='90%';
+console.log(typeof marks);
+
+console.log("********(3)********");
+
+var isWorking = true;
+console.log(typeof isWorking);
+
+console.log("********(4)********");
+
+var totalCount ="hundred and seven"
+console.log(typeof totalCount);
+var totalCount =107;
+console.log(typeof totalCount);
+
+
+
