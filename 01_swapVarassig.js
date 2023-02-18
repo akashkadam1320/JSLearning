04763cee3f304100265d6976c7ccb68cad510161
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-19 01:06:41 +0530

    swapping assigment
    swapping assigment

---
+++ 01_swapVarassig.js
@@ -0,0 +1,20 @@
+console.log("********(swapping variables (1))********");
+var sweety1 = "sweety";
+var cutie1 = "cutie";
+
+var temp = sweety1;
+sweety1 = cutie1;
+cutie1=temp;
+console.log(sweety1,cutie1);
+
+console.log("********(swapping variables (2))********");
+
+var num1 =100;
+var num2 =200;
+var num3 =300;
+var Temp =100;
+
+num1 = num2;
+num2 = num3;
+num3 = Temp;
+console.log(num1,num2,num3);
