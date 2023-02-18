c2c836b6f4f14c3bc293b98be40f4405a43ceafd
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-19 01:05:55 +0530

    variable data types assigement
    variable data types assigement

---
+++ 01_variablebAsieassig.js
@@ -0,0 +1,45 @@
+console.log("********data type number********");
+var PIN = 400072;
+console.log("PINCODE:",typeof PIN);
+
+var ID = 01;
+console.log("ID:",typeof ID);
+
+var contactNo =8652059827;
+console.log("CONTACTNO:",typeof contactNo);
+
+console.log("********data type string********");
+
+var Name = "akash";
+console.log("NAME:",typeof Name);
+
+var addres = "mumbai";
+console.log("ADDRES:",typeof addres);
+
+var ID = "02";
+console.log("ID:",typeof ID);
+
+console.log("********data type boolean********");
+
+var isLazy = true;
+console.log("data type :" ,typeof isLazy);
+
+var isMarried = false;
+console.log("data type", typeof isMarried);
+
+var iscrazy = false;
+console.log("data type:",typeof iscrazy);
+
+console.log("********undefind********");
+
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", graduation);
+
+console.log("********Null********");
+
+var city=null;
+console.log("Value of  city variable is: ", city);
+console.log("typeOf city is: ", typeof city);
+
