c959e6561cc7d72b8071881a25de7f3f0ba782c7
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-22 14:11:16 +0530

    update code
    update code

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+console.log("Akash Kadam");
+console.log(32);
+console.log(30*5);
+console.log(30+5-3);
+var fullName="Akash Kadam"
+console.log(fullName);
+var city;
+console.log(city);
---
+++ 04_stringTemplate.js
@@ -0,0 +1,7 @@
+console.log(`you are"champ" man`);
+var firstName="akash";
+var lastName="kadam"
+city="mumbai";
+
+console.log("My First name ", firstName, "Last name ", lastName, " City ", city);
+console.log(`my first name ${firstName} last name ${lastName} city ${city}`);
