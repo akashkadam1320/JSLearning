ac6ec15c9f854bd7729e48fd8fdb879d1ad32322
akashkadam1320 <akashkadam1320@gmail.com>
2023-02-22 13:31:21 +0530

    string function assignment
    string function assignment

---
+++ 04_stringAssig01.js
@@ -0,0 +1,16 @@
+var hobby1 = "Playing Cricket";
+var hobby2 = "Sweaming";
+var hobby3 = "Bike Riding";
+
+function stringBasics() {
+    console.log("My Dream Company Name: J.P.MORGAN");
+    console.log("----------------------------------------");
+    console.log("My Hobbies:",hobby1,hobby2,hobby3);
+    console.log("total sum of hobby1 char:",hobby1.length);
+    console.log("total sum of hobby2 char:",hobby2.length);
+    console.log("total sum of hobby3 char:",hobby3.length);
+
+    console.log("----------------------------------------");
+    console.log("total sum of character avaliable is:",hobby1.length + hobby2.length + hobby3.length);
+}
+stringBasics();
