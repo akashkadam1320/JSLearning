e177134370227975793aa0b27ae0074b17fa8925
akashkadam1320 <akashkadam1320@gmail.com>
2023-03-01 20:14:29 +0530

    for loop assignment
    for loop assignment

---
+++ 05_incrementDecrementOpt.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+// index++ ==> index = index +1 
+// index-- ==> index = index -1
+
+var index = 10 ;
+var y = index++  // 11
+console.log(index,y);  // output 10 11
+
+console.log("**********************************");
+
+var index = 10 ;
+var y = index--  
+console.log(`index:${index}, y:${y}`);      
---
+++ 06_for_loop.js
@@ -0,0 +1,41 @@
+
+
+
+// Print Number From 1 to 10
+// initailization 1
+// update Expresion +1
+// condition <=10
+for (let index = 1; index <=10; index =index+1) { // index = 1 conditon 1<=10
+    console.log(index);
+    
+}
+
+
+// Print Number from 0 to 5 
+// / initailization 0
+// update Expresion +1
+// condition <5
+for (let index = 0; index <5;index= index+1) {
+console.log(index);    
+}
+
+console.log("reverse number");
+// Print Number from 10 to 0 
+// / initailization 10
+// update Expresion +1
+// condition >=10
+   
+for (let index = 10; index >=0; index= index-1) {
+    console.log(index);
+    
+}
+
+
+console.log("table of 5");
+// initailization  condition update Expresion
+
+for (let index = 5; index <=50 ;index = index+5) {
+    console.log(index);
+    
+}
+
