d51e910adc2538c951870fdef544b4fa675684ae
akashkadam1320 <akashkadam1320@gmail.com>
2023-03-06 18:38:43 +0530

    array assigement
    array assigement

---
+++ 07_variable_scope.js
@@ -0,0 +1,29 @@
+
+var firstName; // variable declrartion
+firstName = "virat";  // variable initilaization
+
+var lastName = "kohli";  //variable declrartion and initilaization
+firstName = "king kohli";  //variable update
+
+var firstName; // variable re-declaration
+console.log(firstName);
+
+let city; //  variable declrartion
+city = "delhi"; //variable initilaization
+
+// city = "mumbai"; // variable re-declaration
+
+const PI = 3.14;
+// PI = 3.56;  // update or modify not allowed
+console.log(PI);
+// const PI = 3.1412;  // re-declaration of const variable is not allowed
+
+var sum = 10;
+if(num==10){
+    let word = "Revision is the mother of success";
+}
+console.log(word);
+
+function sayHi(){
+    
+}
